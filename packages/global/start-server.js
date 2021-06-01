28d7285872f927a26c699004a3d5217b77109e06
Brian Miller <brian@parameter1.com>
2021-06-01 13:19:49 -0500

    Add html sitemap routes & pagination

--- packages/global/start-server.js
+++ packages/global/start-server.js
@@ -2,6 +2,8 @@ const newrelic = require('newrelic');
 const { startServer } = require('@parameter1/base-cms-marko-web');
 const { set, get, getAsObject } = require('@parameter1/base-cms-object-path');
 const cleanResponse = require('@parameter1/base-cms-marko-core/middleware/clean-marko-response');
+const htmlSitemapPagination = require('@parameter1/base-cms-marko-web-html-sitemap/middleware/paginated');
+const htmlSitemapRoutes = require('@parameter1/base-cms-marko-web-html-sitemap/routes');
 
 const document = require('./components/document');
 const components = require('./components');
@@ -10,6 +12,8 @@ const fragments = require('./fragments');
 const buildNativeXConfig = require('./native-x/build-config');
 
 const routes = siteRoutes => (app) => {
+  // HTML Sitemap
+  htmlSitemapRoutes(app);
   // Load site routes.
   siteRoutes(app);
 };
@@ -37,6 +41,9 @@ module.exports = (options = {}) => {
       // Setup IdentityX.
       const identityXConfig = get(options, 'siteConfig.identityX');
       set(app.locals, 'identityX', identityXConfig);
+
+      // Use paginated middleware
+      app.use(htmlSitemapPagination());
 
       // Clean all response bodies.
       app.use(cleanResponse());
