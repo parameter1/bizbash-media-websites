8dbaafb420064d6d755ed976d7c940de06253789
Brian Miller <brian@parameter1.com>
2023-01-23 12:34:22 -0600

    Remove shared pacakge & old.bizbash.com files and refs

--- packages/shared/routes/index.js
+++
@@ -1,9 +0,0 @@
-const search = require('./search');
-
-module.exports = (app, siteConfig) => {
-  // IdentityX (user routing and app context)
-  // identityX(app);
-
-  // Search routes
-  search(app, siteConfig);
-};
