791e73ef19e2782745b76a6e9b69bb77510207c6
Brian Miller <brian@parameter1.com>
2024-01-11 10:35:40 -0600

    Add env prop to turn on injected body links

--- packages/global/graphql/fragments/content-page.js
+++ packages/global/graphql/fragments/content-page.js
@@ -1,223 +1,229 @@
 const gql = require('graphql-tag');
 
-module.exports = gql`
-fragment ContentPageFragment on Content {
-  id
-  name
-  labels
-  teaser(input: { useFallback: false, maxLength: null })
-  body
-  status
-  published
-  siteContext {
-    path
-  }
-  company {
-    id
-    name
-    siteContext {
-      path
-    }
-  }
-  taxonomy(input: { type: Category }) {
-    edges {
-      node {
+const factory = ({ useLinkInjectedBody = false } = {}) => {
+  const fragment = gql`
+    fragment ContentPageFragment on Content {
+      id
+      name
+      labels
+      teaser(input: { useFallback: false, maxLength: null })
+      body(input: { useLinkInjectedBody: ${useLinkInjectedBody} })
+      status
+      published
+      siteContext {
+        path
+      }
+      company {
         id
         name
+        siteContext {
+          path
+        }
+      }
+      taxonomy(input: { type: Category }) {
+        edges {
+          node {
+            id
+            name
+            hierarchy {
+              id
+              name
+            }
+          }
+        }
+      }
+      primarySection {
+        id
+        name
+        alias
+        canonicalPath
         hierarchy {
           id
           name
+          alias
+          canonicalPath
         }
       }
-    }
-  }
-  primarySection {
-    id
-    name
-    alias
-    canonicalPath
-    hierarchy {
-      id
-      name
-      alias
-      canonicalPath
-    }
-  }
-  primaryImage {
-    id
-    src(input: { useCropRectangle: true, options: { auto: "format,compress", q: 70 } })
-    alt
-    caption
-    credit
-    isLogo
-    cropDimensions {
-      aspectRatio
-    }
-    primaryImageDisplay
-  }
-  images(input:{ pagination: { limit: 100 }, sort: { order: values } }) {
-    edges {
-      node {
+      primaryImage {
         id
-        src(input: { options: { auto: "format,compress", q: 70 } })
+        src(input: { useCropRectangle: true, options: { auto: "format,compress", q: 70 } })
         alt
-        displayName
         caption
         credit
         isLogo
+        cropDimensions {
+          aspectRatio
+        }
+        primaryImageDisplay
       }
-    }
-  }
-  companies: relatedContent(input: { includeContentTypes: [Company] }) {
-    edges {
-      node {
-        id
+      images(input:{ pagination: { limit: 100 }, sort: { order: values } }) {
+        edges {
+          node {
+            id
+            src(input: { options: { auto: "format,compress", q: 70 } })
+            alt
+            displayName
+            caption
+            credit
+            isLogo
+          }
+        }
       }
-    }
-  }
-  gating {
-    surveyType
-    surveyId
-  }
-  userRegistration {
-    isCurrentlyRequired
-    accessLevels
-  }
-  ... on ContentVideo {
-    embedCode
-  }
-  ... on ContentNews {
-    source
-    byline
-  }
-  ... on ContentEvent {
-    endDate
-    startDate
-  }
-  ... on ContentArticle {
-    sidebars
-  }
-  ... on ContentTopList {
-    relatedTo(input:{ pagination: { limit: 100 }, sort: { order: values } }) {
-      edges {
-        node {
-          id
-          type
-          shortName
-          canonicalPath
-          images(input:{ pagination: { limit: 100 }, sort: { order: values } }) {
-            edges {
-              node {
+      companies: relatedContent(input: { includeContentTypes: [Company] }) {
+        edges {
+          node {
+            id
+          }
+        }
+      }
+      gating {
+        surveyType
+        surveyId
+      }
+      userRegistration {
+        isCurrentlyRequired
+        accessLevels
+      }
+      ... on ContentVideo {
+        embedCode
+      }
+      ... on ContentNews {
+        source
+        byline
+      }
+      ... on ContentEvent {
+        endDate
+        startDate
+      }
+      ... on ContentArticle {
+        sidebars
+      }
+      ... on ContentTopList {
+        relatedTo(input:{ pagination: { limit: 100 }, sort: { order: values } }) {
+          edges {
+            node {
+              id
+              type
+              shortName
+              canonicalPath
+              images(input:{ pagination: { limit: 100 }, sort: { order: values } }) {
+                edges {
+                  node {
+                    id
+                    src
+                    alt
+                    displayName
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+      ... on ContentVenue {
+        enableRmi
+        spaces(input:{ pagination: { limit: 25 } }) {
+          edges {
+            node {
+              id
+              name
+              area
+              capacityMin
+              capacityMaxSeated
+              capacityMaxStanding
+              floorPlan {
                 id
                 src
-                alt
-                displayName
+              }
+            }
+          }
+        }
+      }
+      ... on ContentWebinar {
+        linkUrl
+        startDate
+        sponsors {
+          edges {
+            node {
+              id
+              name
+              siteContext {
+                path
+              }
+            }
+          }
+        }
+      }
+      ... on Addressable {
+        address1
+        address2
+        cityStateZip
+        country
+      }
+      ... on Contactable {
+        phone
+        tollfree
+        fax
+        website
+        title
+        mobile
+        publicEmail
+      }
+      ... on ContentCompany {
+        email
+      }
+      ... on SocialLinkable {
+        socialLinks {
+          provider
+          url
+          label
+        }
+      }
+      ... on Media {
+        fileSrc
+      }
+      ... on Authorable {
+        authors {
+          edges {
+            node {
+              id
+              name
+              type
+              siteContext {
+                path
+              }
+            }
+          }
+        }
+        contributors {
+          edges {
+            node {
+              id
+              name
+              type
+              siteContext {
+                path
+              }
+            }
+          }
+        }
+        photographers {
+          edges {
+            node {
+              id
+              name
+              type
+              siteContext {
+                path
               }
             }
           }
         }
       }
     }
-  }
-  ... on ContentVenue {
-    enableRmi
-    spaces(input:{ pagination: { limit: 25 } }) {
-      edges {
-        node {
-          id
-          name
-          area
-          capacityMin
-          capacityMaxSeated
-          capacityMaxStanding
-          floorPlan {
-            id
-            src
-          }
-        }
-      }
-    }
-  }
-  ... on ContentWebinar {
-    linkUrl
-    startDate
-    sponsors {
-      edges {
-        node {
-          id
-          name
-          siteContext {
-            path
-          }
-        }
-      }
-    }
-  }
-  ... on Addressable {
-    address1
-    address2
-    cityStateZip
-    country
-  }
-  ... on Contactable {
-    phone
-    tollfree
-    fax
-    website
-    title
-    mobile
-    publicEmail
-  }
-  ... on ContentCompany {
-    email
-  }
-  ... on SocialLinkable {
-    socialLinks {
-      provider
-      url
-      label
-    }
-  }
-  ... on Media {
-    fileSrc
-  }
-  ... on Authorable {
-    authors {
-      edges {
-        node {
-          id
-          name
-          type
-          siteContext {
-            path
-          }
-        }
-      }
-    }
-    contributors {
-      edges {
-        node {
-          id
-          name
-          type
-          siteContext {
-            path
-          }
-        }
-      }
-    }
-    photographers {
-      edges {
-        node {
-          id
-          name
-          type
-          siteContext {
-            path
-          }
-        }
-      }
-    }
-  }
-}
-`;
+    `;
+  fragment.factory = factory;
+  return fragment;
+};
+
+module.exports = factory();
--- sites/bizbash.com/server/routes/content.js
+++ sites/bizbash.com/server/routes/content.js
@@ -1,10 +1,15 @@
 const withContent = require('@bizbash-media/package-global/middleware/with-content');
-const queryFragment = require('@bizbash-media/package-global/graphql/fragments/content-page');
+const qf = require('@bizbash-media/package-global/graphql/fragments/content-page');
 const contact = require('@bizbash-media/package-global/templates/content/contact');
 const company = require('../templates/content/company');
 const content = require('../templates/content');
 
 module.exports = (app) => {
+  const { site } = app.locals;
+  // base on site config||USE_LINK_INJECTED_BODY to enable bcl
+  const useLinkInjectedBody = site.get('useLinkInjectedBody');
+  const queryFragment = qf.factory ? qf.factory({ useLinkInjectedBody }) : qf;
+
   app.get('/*?contact/:id(\\d{8})*', withContent({
     template: contact,
     queryFragment,
